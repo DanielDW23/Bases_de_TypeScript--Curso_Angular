7b5a877144479f7a3eed8a64758c06ea5599fa17
Daniel <dlopezg4@gmail.com>
2025-07-27 12:08:26 +0200

    Ejercicios y ejemplos hata -- Importaciones y exportaciones

---
+++ src/topics/03-functions.ts
@@ -0,0 +1,44 @@
+
+
+function addNumbers(a: number, b: number): number {
+    return a + b;
+}
+
+const addNumbersArrow = (a: number, b: number): number => {
+    return a + b;
+}
+
+function multiply( firstNumber: number, secondNumber?: number, base: number = 2) {
+    return firstNumber * base;
+}
+
+// const result = addNumbers(1, 2); 
+// const resultArrow = addNumbersArrow(1, 2);
+// const resultMultiply: number = multiply(5);
+// console.log({ result, resultArrow, resultMultiply });// 3)
+
+interface Character {
+    name: string;
+    hp: number;
+    showHp: () => void;
+}
+
+
+
+const healCharacter = ( character: Character, amount: number ) => {
+
+    character.hp += amount;
+}
+
+const strider: Character = {    
+    name: 'Strider',
+    hp: 50,
+    showHp() {
+        console.log(`Puntos de vida ${this.hp}`);
+    }
+}
+
+healCharacter(strider, 20);
+healCharacter(strider, 30);
+
+strider.showHp();
---
+++ src/topics/06-function-destructuring.ts
@@ -0,0 +1,51 @@
+
+interface Product {
+    description: string;
+    price: number;
+}
+
+const phone: Product = {
+    description: 'iPhone 14 Pro',
+    price: 1199.99
+}
+
+const tablet: Product = {
+    description: 'iPad Pro',    
+    price: 899.99
+}
+
+interface TaxCaculationOptions {
+    tax: number;
+    products: Product[];
+}
+
+
+//function taxCalculation( options: TaxCaculationOptions ): [ number, number] {
+//function taxCalculation( { tax, products}: TaxCaculationOptions ): [ number, number] {
+function taxCalculation( options: TaxCaculationOptions ): [ number, number] {
+
+    const { tax, products } = options;
+    
+    let total = 0;
+
+    products.forEach( ({ price }) => {
+        total += price;
+    })
+
+    return [total, total * tax];
+} 
+
+
+const shoppingCart = [phone, tablet];
+const tax = 0.15;
+
+
+const [ total, taxTotal] = taxCalculation({
+    products: shoppingCart,
+    // tax: tax
+    tax // ES6 shorthand property name
+})
+
+console.log('Total: ', total);
+console.log('Tax: ', taxTotal);
+
